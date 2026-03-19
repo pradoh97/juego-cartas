f3bb00d5fa94f9267e54ac441f640e6e42cc0ead
pradoh97 <pradoh97@gmail.com>
2026-03-18 22:41:00 -0300

    Add dices multipliers

--- player/player_stats.gd
+++ player/player_stats.gd
@@ -10,11 +10,23 @@ func _ready():
 		ui_node.initialize(stat.modifier)
 		ui_node.set_value(stat.amount, transition)
 
+func throw_dice(amount: int = 1):
+	var throw_value = 0
+
+	for throws in amount:
+		throw_value += randi_range(1, 6)
+
+	return throw_value
+
 func set_stats(selected_card: Card):
 	for stat in selected_card.stats.stat:
 		var modifier_type: Modifier.MODIFIER_TYPE = (stat as Stat).modifier.type
 		var amount: int = (stat as Stat).amount
+
 		for player_stat in stats:
 			if player_stat.modifier.type == modifier_type:
-				player_stat.amount += stat.amount
-				stats[player_stat].set_value(player_stat.amount)
+				var dice_multiplier = throw_dice(abs(stat.amount))
+				player_stat.amount += stat.amount * dice_multiplier
+
+				var ui_node: StatUI = stats[player_stat]
+				ui_node.set_value(player_stat.amount)
