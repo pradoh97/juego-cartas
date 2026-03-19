4bf1ebb60e80ef9a8284585ec311365058ec4cef
pradoh97 <pradoh97@gmail.com>
2026-03-19 19:55:28 -0300

    Changed dictionary definition to reduce complexity of search

--- player/player_stats.gd
+++ player/player_stats.gd
@@ -1,13 +1,18 @@
 extends MarginContainer
 class_name PlayerStats
 
-@export var stats: Dictionary[Stat, StatUI]
+signal stat_changed(modifier_type: Modifier.MODIFIER_TYPE, amount: int)
+
+@export var stats_ui: Dictionary[Modifier.MODIFIER_TYPE, StatUI]
+@export var stats: Dictionary[Modifier.MODIFIER_TYPE, Stat]
 
 func _ready():
 	visible = false
-	for stat in stats:
-		var ui_node = stats[stat]
+	for modifier_type in stats:
+		var ui_node: StatUI = stats_ui[modifier_type]
 		var transition: bool = false
+		var stat: Stat = stats[modifier_type]
+
 		ui_node.initialize(stat.modifier)
 		ui_node.set_value(stat.amount, transition)
 
@@ -20,17 +25,15 @@ func throw_dice(amount: int = 1):
 	return throw_value
 
 func set_stats(selected_card: Card):
-	for stat in selected_card.stats.stat:
-		var modifier_type: Modifier.MODIFIER_TYPE = (stat as Stat).modifier.type
-		var amount: int = (stat as Stat).amount
-
-		for player_stat in stats:
-			if player_stat.modifier.type == modifier_type:
-				var dice_multiplier = throw_dice(abs(stat.amount))
-				player_stat.amount += stat.amount * dice_multiplier
-
-				var ui_node: StatUI = stats[player_stat]
-				ui_node.set_value(player_stat.amount)
+	for card_stat in selected_card.stats.stat:
+		var modifier_type = card_stat.modifier.type
+		var ui_node: StatUI = stats_ui[modifier_type]
+		var transition: bool = false
+		var dice_multiplier = throw_dice(abs(card_stat.amount))
+		var new_value = stats[modifier_type].amount + sign(card_stat.amount) * dice_multiplier
+		stats[modifier_type].amount = new_value
+		ui_node.set_value(new_value, transition)
+		stat_changed.emit(modifier_type, new_value)
 
 func update_day_turn(day: int, turn: int):
 	%Day.set_value(day)
