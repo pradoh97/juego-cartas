0057a5b536d930a8c547f33def642cf1048a98a1
pradoh97 <pradoh97@gmail.com>
2026-03-21 18:48:35 -0300

    Add method to execute condition events

--- stats/stat_condition_system.gd
+++ stats/stat_condition_system.gd
@@ -3,8 +3,8 @@ extends Node
 @export var conditions: Array[StatCondition]
 var context: Context = Context.new()
 
-func event_conditions_met(stats: StatsCollection) -> bool:
-	var conditions_met: Array = []
+func event_conditions_met(stats: StatsCollection) -> Array[StatCondition]:
+	var conditions_met: Array[StatCondition] = []
 	for stat in stats.stats:
 		var condition_index = conditions.find_custom(func(condition): return condition.modifier_type == stat.modifier.type)
 		var condition_met: bool = false
@@ -15,5 +15,8 @@ func event_conditions_met(stats: StatsCollection) -> bool:
 			condition_met = expression.execute()
 		if condition_met:
 			conditions_met.append(conditions[condition_index])
-			conditions[condition_index].event_to_trigger.execute(context)
-	return conditions_met.size()
+	return conditions_met
+
+func execute_condition_event(conditions_to_execute: Array[StatCondition]):
+	for condition in conditions_to_execute:
+		condition.event_to_trigger.execute(context)
