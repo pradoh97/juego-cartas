be0be72104265b460d7bb523cb8dc3adb939d4c5
pradoh97 <pradoh97@gmail.com>
2026-03-31 21:21:00 -0300

    Fix bug on multiple conditions

--- stats/stat_condition_system.gd
+++ stats/stat_condition_system.gd
@@ -5,17 +5,21 @@ var context: Context = Context.new()
 
 func event_conditions_met(stats: StatsCollection) -> Array[StatCondition]:
 	var conditions_met: Array[StatCondition] = []
-	for stat in stats.stats:
-		var condition_index = conditions.find_custom(func(condition): return condition.modifier_type == stat.modifier.type)
-		var condition_met: bool = false
 
-		if condition_index > -1:
-			var expression = Expression.new()
-			var condition: StatCondition = conditions[condition_index]
-			expression.parse(str(stat.amount) + condition.operator + str(condition.threshold))
-			condition_met = expression.execute()
+	for stat in stats.stats:
+		var conditions_found = conditions.filter(func(condition): return condition.modifier_type == stat.modifier.type)
+		conditions_met.append_array(get_conditions_met(conditions_found, stat))
+
+	return conditions_met
+
+func get_conditions_met(conditions_to_evaluate: Array[StatCondition], stat: Stat):
+	var conditions_met: Array[StatCondition] = []
+	for condition in conditions_to_evaluate:
+		var expression = Expression.new()
+		expression.parse(str(stat.amount) + condition.operator + str(condition.threshold))
+		var condition_met = expression.execute()
 		if condition_met:
-			conditions_met.append(conditions[condition_index])
+			conditions_met.append(condition)
 	return conditions_met
 
 func execute_condition_event(conditions_to_execute: Array[StatCondition]):
